3cff197edd41897e22609b602803df30e480f601
flightlessbird <102932289+alexflightlessbird@users.noreply.github.com>
2023-06-29 05:56:47 -0400

    presetHandler.js update
    Local version of script.js re-loaded, no changes

--- presetHandler.js
+++ presetHandler.js
@@ -2,7 +2,28 @@
 const presets = require('./presets');
 
 // Complete this function:
-const presetHandler = () => {};
+const presetHandler = (method, index, newPresetArray) => {
+    let responseArr = [];
+
+    if (method == "GET") {
+        if (presets[index]) {
+            responseArr = [200, presets[index]];
+        } else {
+            responseArr[0] = 404;
+        }
+    } else if (method == "PUT") {
+        if (presets[index]) {
+            presets[index] = newPresetArray;
+            responseArr = [200, newPresetArray];
+        } else {
+            responseArr[0] = 404;
+        }
+    } else {
+        responseArr[0] = 400;
+    }
+
+    return responseArr;
+};
 
 // Leave this line so that your presetHandler function can be used elsewhere:
 module.exports = presetHandler;
